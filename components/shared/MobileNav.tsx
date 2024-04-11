8dbc8dc8133731ee8a1ce043cb8076575f1328df
RW-AI <73473969+rasmuswind-ai@users.noreply.github.com>
2024-04-11 20:50:37 +0200

    modified:   app/(root)/layout.tsx 	new file:   components/shared/MobileNav.tsx 	new file:   components/ui/sheet.tsx 	modified:   package-lock.json 	modified:   package.json

---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,49 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+import { SignedIn, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+  
+
+const MobileNav = () => {
+  return (
+    <header className="header">
+        <Link href="/" className="flex items-center gap-2 md:py-2">
+            <Image 
+                src="/assets/images/logo-text.svg"
+                alt="logo" 
+                width={180} 
+                height={28} 
+            />
+        </Link>
+        <nav className="flex gap-2">
+            <SignedIn>
+                <UserButton afterSignOutUrl="/" />
+
+                <Sheet>
+                    <SheetTrigger>
+                        <Image 
+                            src="/assets/icons/menu.svg"
+                            alt="menu"
+                            width={32}
+                            height={32}
+                            className="cursor-pointer"
+                        />
+                    </SheetTrigger>
+                    <SheetContent className="sheet-content sm:w-64">
+                        
+                    </SheetContent>
+                </Sheet>
+            </SignedIn>
+        </nav>
+    </header>
+  )
+}
+
+export default MobileNav
