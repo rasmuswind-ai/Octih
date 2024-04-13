ed1371bfe4fbaee609a111e06a0d28f7e3784155
RW-AI <73473969+rasmuswind-ai@users.noreply.github.com>
2024-04-13 20:37:08 +0200

    modified:   middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk'
-  ]
+  publicRoutes: ['/'],
+  ignoredRoutes: ['/api/webhooks/clerk'],
 });
  
 export const config = {
