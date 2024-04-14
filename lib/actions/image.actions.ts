89cfa2a5d12aed3825ab7ad59c2568e8ed78ef8c
RW-AI <73473969+rasmuswind-ai@users.noreply.github.com>
2024-04-14 20:47:18 +0200

    modified:   lib/actions/image.actions.ts

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -3,15 +3,28 @@
 import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
+import User from "../database/models/user.model";
+import Image from "../database/models/image.model";
 
 // ADD IMAGE
 export async function addImage({ image, userId, path }: AddImageParams) {
     try {
         await connectToDatabase();
 
+        const author = await User.findById(userId);
+
+        if (!author) {
+            throw new Error("User not found");
+        }
+
+        const newImage = await Image.create({
+            ...image,
+            author: author._id
+        })
+
         revalidatePath(path);
 
-        return JSON.parse(JSON.stringify(image));
+        return JSON.parse(JSON.stringify(newImage));
     } catch (error) {
         handleError(error)
     }
