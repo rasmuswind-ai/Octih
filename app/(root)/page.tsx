0aa58ca5a1940616bedc0c24bedfdceec5bc3f49
RW-AI <73473969+rasmuswind-ai@users.noreply.github.com>
2024-04-15 17:17:14 +0200

    modified:   app/(root)/page.tsx 	modified:   app/(root)/transformations/[id]/page.tsx 	new file:   components/shared/DeleteConfirmation.tsx 	modified:   components/shared/TransformedImage.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,15 @@
 import { Collection } from "@/components/shared/Collection"
 import { navLinks } from "@/constants"
+import { getAllImages } from "@/lib/actions/image.actions"
 import Image from "next/image"
 import Link from "next/link"
 
-const Home = ({ searchParams }: SearchParamProps) => {
+const Home = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || '';
-  
+
+  const images = await getAllImages({ page, searchQuery})
+
   return (
     <>
       <section className="home">
@@ -30,7 +33,12 @@ const Home = ({ searchParams }: SearchParamProps) => {
       </section>
 
       <section className="sm:mt-12">
-        <Collection />
+        <Collection 
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
       </section>
     </>
   )
