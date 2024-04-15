4b2d22240dafd1faddc7115e39d2258a1bb8e38a
RW-AI <73473969+rasmuswind-ai@users.noreply.github.com>
2024-04-15 17:07:53 +0200

    modified:   app/(root)/page.tsx 	new file:   components/shared/Collection.tsx 	new file:   components/shared/Search.tsx 	new file:   components/ui/pagination.tsx modified:   lib/actions/image.actions.ts 	modified:   package-lock.json 	modified:   package.json

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,12 @@
+import { Collection } from "@/components/shared/Collection"
 import { navLinks } from "@/constants"
 import Image from "next/image"
 import Link from "next/link"
 
-const Home = () => {
+const Home = ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || '';
+  
   return (
     <>
       <section className="home">
@@ -23,6 +27,10 @@ const Home = () => {
             </Link>
           ))}
         </ul>
+      </section>
+
+      <section className="sm:mt-12">
+        <Collection />
       </section>
     </>
   )
